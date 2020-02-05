5f6b91d012cf73fe96ea0d87ee8410adc5187056
Josemolamazo <Jreyarijon@danielcastelao.org>
2020-02-05 14:34:38 +0100

    Inicializada la conexión al la información html de la pagina web del tiempo

--- src/main/java/ElTiempo.java
+++ src/main/java/ElTiempo.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 
 public class ElTiempo {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello");
+        /* System.out.println("Hello");
         Document doc = Jsoup.connect("http://www.google.com").userAgent("Mozilla").get();
         Element miElemento = doc.select("a").first();
         String  miTexto = miElemento.text();
-        System.out.println(miTexto);
+        System.out.println(miTexto); */
+
+        Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Chrome").get();
 
     }
 }
