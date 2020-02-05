601be207d0ef9f48b1e44c6b1db5dd1322c04185
Josemolamazo <Jreyarijon@danielcastelao.org>
2020-02-05 14:57:52 +0100

    Ejercicio de gradel con visualización completa

--- src/main/java/ElTiempo.java
+++ src/main/java/ElTiempo.java
@@ -15,8 +15,12 @@ public class ElTiempo {
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
 
         Elements miElemento = doc.getElementsByClass("today_nowcard-temp");
+        Elements miElemento1 = doc.getElementsByClass("today_nowcard-phrase");
+        String titulo = doc.title();
         String miTexto = miElemento.text();
-        System.out.println(miTexto);
+        String miTexto1 = miElemento1.text();
+        String miTexto2 = titulo;
+        System.out.println("Hoy está "+miTexto1 +"y hace "+miTexto+" grados. Datos extraídos de Vigo, España Pronóstico del tiempo y condiciones meteorológicas "+miTexto2);
 
     }
 }
